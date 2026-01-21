9d89dc170a107d0a0abb1330698721831889f59f
João Lucas Silva <collectivenahual@gmail.com>
2026-01-21 17:25:16 +0000

    File changes

--- src/pages/VistoriarUnidade.jsx
+++ src/pages/VistoriarUnidade.jsx
@@ -220,7 +220,7 @@ export default function VistoriarUnidade() {
                         observacao: data.observacao
                     });
                     
-                    // Incrementar apenas o contador de constatações
+                    // Incrementar apenas o contador de constatações (a partir da 2ª resposta)
                     setContadores(prev => ({
                         ...prev,
                         C: prev.C + 1
