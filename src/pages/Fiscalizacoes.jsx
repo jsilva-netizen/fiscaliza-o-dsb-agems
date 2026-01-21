0ecde063b101da2a46d6d4e0df5fc73627b5ad3f
João Lucas Silva <collectivenahual@gmail.com>
2026-01-21 16:14:23 +0000

    File changes

--- src/pages/Fiscalizacoes.jsx
+++ src/pages/Fiscalizacoes.jsx
@@ -18,6 +18,7 @@ import { format } from 'date-fns';
 import { ptBR } from 'date-fns/locale';
 import RelatorioFiscalizacao from '@/components/fiscalizacao/RelatorioFiscalizacao';
 import ExportarPDFConsolidado from '@/components/fiscalizacao/ExportarPDFConsolidado';
+import ResumoFiscalizacao from '@/components/fiscalizacao/ResumoFiscalizacao';
 
 export default function Fiscalizacoes() {
     const queryClient = useQueryClient();
